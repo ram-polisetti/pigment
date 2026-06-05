5ce323691c59db99d5aa7bd86dca849a80b1bd8c
Ram <ram@Mac.lan>
2026-06-04 21:05:00 -0400

    Apply refined Atelier studio identity

--- PinPin/Utilities/AppPalette.swift
+++ PinPin/Utilities/AppPalette.swift
@@ -1,23 +1,24 @@
 import SwiftUI
 
 enum AppPalette {
-    static let deepCharcoal = Color(red: 0.122, green: 0.122, blue: 0.137)
-    static let warmOffWhite = Color(red: 0.961, green: 0.953, blue: 0.937)
-    static let mutedIndigo = Color(red: 0.298, green: 0.353, blue: 0.447)
-    static let dustyTerracotta = Color(red: 0.769, green: 0.416, blue: 0.290)
-    static let oliveGreen = Color(red: 0.478, green: 0.545, blue: 0.353)
-    static let softOchre = Color(red: 0.839, green: 0.659, blue: 0.353)
-    static let coolGray = Color(red: 0.659, green: 0.659, blue: 0.678)
+    static let warmLinen = Color(red: 0.957, green: 0.941, blue: 0.910)
+    static let gessoWhite = Color(red: 0.984, green: 0.973, blue: 0.953)
+    static let atelierBlack = Color(red: 0.153, green: 0.153, blue: 0.149)
+    static let atelierBlue = Color(red: 0.086, green: 0.216, blue: 0.290)
+    static let yellowOchre = Color(red: 0.784, green: 0.604, blue: 0.239)
+    static let burntSienna = Color(red: 0.659, green: 0.361, blue: 0.243)
+    static let umberOlive = Color(red: 0.431, green: 0.412, blue: 0.322)
+    static let stoneGray = Color(red: 0.718, green: 0.690, blue: 0.643)
 
-    static let vanDykeBrown = deepCharcoal
-    static let rawUmber = mutedIndigo
-    static let titaniumWhite = warmOffWhite
-    static let warmTitaniumWhite = warmOffWhite
-    static let mauve = mutedIndigo
-    static let mutedMauve = dustyTerracotta
-    static let surface = warmOffWhite
-    static let raisedSurface = Color(red: 0.985, green: 0.979, blue: 0.965)
-    static let selectedSurface = softOchre.opacity(0.26)
-    static let hairline = coolGray.opacity(0.30)
-    static let secondaryText = mutedIndigo.opacity(0.76)
+    static let vanDykeBrown = atelierBlack
+    static let rawUmber = atelierBlue
+    static let titaniumWhite = warmLinen
+    static let warmTitaniumWhite = warmLinen
+    static let mauve = atelierBlue
+    static let mutedMauve = burntSienna
+    static let surface = warmLinen
+    static let raisedSurface = gessoWhite
+    static let selectedSurface = yellowOchre.opacity(0.24)
+    static let hairline = stoneGray.opacity(0.32)
+    static let secondaryText = umberOlive.opacity(0.86)
 }
