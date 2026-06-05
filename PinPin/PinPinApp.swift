4a85962ec14f73a03d4da447d1c8902f15e442c4
Ram <ram@Mac.lan>
2026-06-04 21:20:25 -0400

    Recover debug SwiftData store on launch

--- PinPin/PinPinApp.swift
+++ PinPin/PinPinApp.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import SwiftData
+import Foundation
 
 @main
 struct AtelierApp: App {
@@ -9,15 +10,21 @@ struct AtelierApp: App {
         do {
             let schema = Schema([Pin.self, SavedColor.self, Project.self])
 #if DEBUG
-            let config = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+            let config = Self.localModelConfiguration(schema: schema)
+            do {
+                container = try ModelContainer(for: schema, configurations: config)
+            } catch {
+                Self.resetLocalStore()
+                container = try ModelContainer(for: schema, configurations: config)
+            }
 #else
             let config = ModelConfiguration(
                 schema: schema,
                 isStoredInMemoryOnly: false,
                 cloudKitDatabase: .private("iCloud.com.atelier.references")
             )
-#endif
             container = try ModelContainer(for: schema, configurations: config)
+#endif
         } catch {
             fatalError("Failed to create ModelContainer: \(error)")
         }
@@ -30,4 +37,33 @@ struct AtelierApp: App {
                 .tint(AppPalette.mauve)
         }
     }
+
+#if DEBUG
+    private static func localModelConfiguration(schema: Schema) -> ModelConfiguration {
+        ModelConfiguration(
+            schema: schema,
+            url: localStoreURL(),
+            cloudKitDatabase: .none
+        )
+    }
+
+    private static func localStoreURL() -> URL {
+        let applicationSupportURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        try? FileManager.default.createDirectory(at: applicationSupportURL, withIntermediateDirectories: true)
+        return applicationSupportURL.appending(path: "AtelierLocal.store")
+    }
+
+    private static func resetLocalStore() {
+        let storeURL = localStoreURL()
+        let fileURLs = [
+            storeURL,
+            URL(fileURLWithPath: storeURL.path + "-shm"),
+            URL(fileURLWithPath: storeURL.path + "-wal")
+        ]
+
+        for fileURL in fileURLs {
+            try? FileManager.default.removeItem(at: fileURL)
+        }
+    }
+#endif
 }
